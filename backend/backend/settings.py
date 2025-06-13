49421b67d70321f0b7ce30480d49510b26ad01f3
Radu <raducanu13@gmail.com>
2025-06-14 01:44:41 +0300

    Setup for render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://duo-plus-plus-frontend.onrender.com",
+    "https://duolingo-plus-plus.onrender.com",
 ]
