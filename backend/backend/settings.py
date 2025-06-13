ae87db247a062be2ce2360d0b858c9ac47d0743f
Radu <raducanu13@gmail.com>
2025-06-14 02:24:16 +0300

    Fix (hopefully) for CORS origin issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOW_ALL_ORIGINS = True # for dev only
+CORS_ALLOW_ALL_ORIGINS = True # for dev only
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,6 +1,7 @@
 from rest_framework.parsers import MultiPartParser
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
